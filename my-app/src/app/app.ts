3a1eb3110b8816804d21d826887e6548cb417f3c
Noragiri <szymonkowalewski.97@gmail.com>
2026-08-26 18:26:40 +0200

    swap from Angular 22 to Angular 20

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,7 +1,12 @@
-import {Component} from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: ` Books :D`,
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('my-app');
+}
